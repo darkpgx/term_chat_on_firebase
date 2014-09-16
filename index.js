9b90def72be6fb41e134c004eee526b10dd98239
darkpgx <darkpgx@gmail.com>
2014-09-15 23:04:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+//Starting Message.
+console.log("Starting Terminal Chat");
+
+//The code for terminal chat starts here
+
+//Required modules
+var http = require('http');
+var prompt = require('prompt');
+var request = require('request');
+prompt.start();
+var Notification = require('node-notifier');
+var notifier = new Notification();
+var Firebase = require('firebase');
+var fb = new Firebase('https://dazzling-inferno-9961.firebaseio.com/');
+//End required modules
+
+//Set up global vars to strings
+var username = '',
+    roomname = '',
+    password = '',
+    user_id = 0,
+    chat_array = [],
+    my_inter,
+    message_counter = 0;
+//End set up global vars
+
+//Error Handler
+function onErr(err) {
+  console.log(err);
+  return 1;
+};
+//End Error Handler
+
+//Prompt for credentials
+prompt.get(['username', 'roomname', 'password'], function (err, result) {
+  if(err) {return onErr(err);};
+  console.log('  username: ' + result.username); //display user entered credentials
+  console.log('  roomname: ' + result.roomname);
+  console.log('  password: ' + result.password);
+  username = result.username;
+  roomname = result.roomname;
+  password = result.password;
+
+  //join or create
+  join(username, password, roomname);
+
+});
+
+//request for existing messages and prints on console
+var print_chat = function(arr) {
+  if (arr.length < 1) {return 0;}
+  for (var j = message_counter; j< arr.length; j++) {
+    if (arr[j]["username"] !== username && message_counter !== 0){
+      notifier.notify({
+        title: 'Terminal message received',
+        message: arr[j]["username"] + ": " + arr[j]["msg"],
+        sound: 'Funk'
+      });
+      console.log('' + arr[j]["username"] + ": " + arr[j]["msg"]);
+    };
+  };
+  message_counter = arr.length;
+};
+
+//Chat function
+var chat = function(username, password, roomname, user_id) {
+  my_inter = setInterval(function(){print_chat(chat_array);}, 500);
+  prompt.message = '';
+  prompt.delimiter = '';
+  prompt.get({properties: {':': {'hidden': true}}}, function (err, result) {
+    clearInterval(my_inter);
+    prompt.get(['Enter chat message: '], function (err, result) {
+      if(result['Enter chat message: '] == 'exit') {
+        fb.child(roomname).child(password).child(user_id).update({msg: username + " has exit the room"}, 
+          function(){process.exit()});
+      } else {
+        var msg = result['Enter chat message: '];
+        fb.child(roomname).child(password).child(user_id).update({msg: msg});
+        chat(username, password, roomname, user_id);
+      }
+    });
+  });
+};
+
+//join function
+var join = function(username, password, roomname) {
+  fb.child(roomname).once('value', function(dat){
+    if (dat.val() == null || password in dat.val()) {
+      joining();
+    }
+    else if(!(password in dat.val())) {
+      console.log('Wrong Password');
+      process.exit();
+    }
+  });
+};
+
+var joining = function () {
+  fb.child(roomname).child(password).once('value', function(dat){
+    user_id = dat.numChildren();
+    console.log("Join room: " + roomname + " with password: " + password);
+    fb.child(roomname).child(password).child(user_id).update({
+      username: username, msg: username + " has joined room: " + roomname
+    });
+    fb.child(roomname).child(password).on('child_changed', function(current, oldName){
+      chat_array.push({username: current.val().username, msg: current.val().msg});
+    });
+    chat(username, password, roomname, user_id);
+  });
+};
